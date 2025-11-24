8269255b62d8654bdace3ed0aa4ee1b28c924135
Khoi Nguyen <nguyentienkhoi22062005@gmail.com>
2025-11-24 12:04:39 +0700

    Change theme default

--- src/hooks/useTheme.ts
+++ src/hooks/useTheme.ts
@@ -3,24 +3,25 @@ import { useEffect, useState } from "react";
 type Theme = "light" | "dark";
 
 function useTheme() {
-  const [theme, setTheme] = useState<Theme>(
-    (localStorage.getItem("theme") as Theme) || "light",
-  );
+    const [theme, setTheme] = useState<Theme>(
+        (localStorage.getItem("theme") as Theme) || "dark"
+    );
 
-  useEffect(() => {
-    const root = document.documentElement;
-    if (theme === "dark") {
-      root.classList.add("dark");
-    } else {
-      root.classList.remove("dark");
-    }
+    useEffect(() => {
+        const root = document.documentElement;
+        if (theme === "dark") {
+            root.classList.add("dark");
+        } else {
+            root.classList.remove("dark");
+        }
 
-    localStorage.setItem("theme", theme);
-  }, [theme]);
+        localStorage.setItem("theme", theme);
+    }, [theme]);
 
-  const toggleTheme = () => setTheme((t) => (t === "dark" ? "light" : "dark"));
+    const toggleTheme = () =>
+        setTheme((t) => (t === "dark" ? "light" : "dark"));
 
-  return { theme, setTheme, toggleTheme };
+    return { theme, setTheme, toggleTheme };
 }
 
 export { useTheme };
